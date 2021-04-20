c2633118dcb7400df38d61d738892076dc23e73c
OSFrog <tommy_8786@hotmail.com>
2021-04-20 20:39:52 +0200

    added buttons

--- scripts/app.js
+++ scripts/app.js
@@ -2,21 +2,21 @@ const mainContainer = document.querySelector('.main-container');
 const mainCenterContainer = document.querySelector('.main-center-container');
 
 
-function getCourses(){
+function getCourses() {
     fetch('http://localhost:3000/courses')
-    .then((response) => {
-        if(!response.ok){
-            throw new Error(response.statusText);
-        }
-        return response.json();
-    })
-    .then(data => {
-        mainContainer.innerHTML = '';
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error(response.statusText);
+            }
+            return response.json();
+        })
+        .then(data => {
+            mainContainer.innerHTML = '';
 
-        for (let obj of data){
-            
-            mainContainer.insertAdjacentHTML("beforeend", 
-                `
+            for (let obj of data) {
+
+                mainContainer.insertAdjacentHTML("beforeend",
+                    `
                 <div class="card-container">
                     <div class="card-image-container">
                     <img src="${obj.image}" />
@@ -26,14 +26,15 @@ function getCourses(){
                         <p>Category: ${obj.category}</p>
                         <p>${obj.description}</p>
                         <p>Length: ${obj.length} weeks</p>
-                    </div>
+                        </div>
+                    <button class="btn">Add To Cart</button>
                 </div>
                 `);
-        }    
-    })
-    .catch((err) => {
-       mainCenterContainer.innerHTML += `<p>${err}</p>`; 
-    })
+            }
+        })
+        .catch((err) => {
+            mainCenterContainer.innerHTML += `<p>${err}</p>`;
+        })
 }
 
 getCourses();
